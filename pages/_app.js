3a18bd5d0197a64a6de7d15dda1a01105c2afe42
bartogamer84 <emiliolicerio@gmail.com>
2026-04-21 10:59:59 -0600

    Fix CSS and fonts loading, add global styles, favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,18 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
